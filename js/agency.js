d2f0f618a572e4524e9e456fa45ad45844ee499f
Daric <tesked@outlook.com>
2016-01-21 23:17:34 -0600

    setup, instagram integration started, going good

--- js/agency.js
+++ js/agency.js
@@ -5,13 +5,25 @@
  */
 
 $(document).ready(function(){
-    i = 1;
-    $("#images").append('<div class="item active"><img src="img/about/' + i + '.jpg"></div>');
-    i++;
-    while (i < 5){
-        $("#images").append('<div class="item"><img src="img/about/' + i + '.jpg"></div>');
-        i++;
-    }
+
+    var feed = new Instafeed({
+        accessToken: '2712500769.5b9e1e6.e19a31b2db164ef0bffbaa35a7024f02',
+        get: 'user',
+        userId:'207426984',
+        sortBy: 'most-recent',
+        // filter: function(image) {
+        //     return image.tags.indexOf('tbt') >= 0;
+        // }
+        template: '<a class="animation" href="{{link}}"><img src="{{image}}" /></a>'
+    });
+    feed.run();
+    // i = 1;
+    // $("#images").append('<div class="item active"><img src="img/about/' + i + '.jpg"></div>');
+    // i++;
+    // while (i < 5){
+    //     $("#images").append('<div class="item"><img src="img/about/' + i + '.jpg"></div>');
+    //     i++;
+    // }
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -34,3 +46,102 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+
+// Google Maps Scripts
+// When the window has finished loading create our google map below
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+  // Basic options for a simple Google Map
+  // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+  var mapOptions = {
+    // How zoomed in you want the map to start at (always required)
+    zoom: 15,
+
+    // The latitude and longitude to center the map (always required)
+    center: new google.maps.LatLng(45.2618, -94.1240), // Carroll
+
+    // Disables the default Google Maps UI components
+    disableDefaultUI: true,
+    scrollwheel: false,
+    draggable: false,
+
+    // How you would like to style the map.
+    // This is where you would paste any style found on Snazzy Maps.
+    styles: [
+        {
+            "featureType": "administrative",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.text",
+            "stylers": [
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.text",
+            "stylers": [
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "labels.text",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        }
+    ]
+  };
+
+  // Get the HTML DOM element that will contain your map
+  // We are using a div with id="map" seen below in the <body>
+  var mapElement = document.getElementById('map');
+
+  // Create the Google Map using out element and options defined above
+  var map = new google.maps.Map(mapElement, mapOptions);
+
+  // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
+   var image = 'img/mapMarker.png';
+   var myLatLng = new google.maps.LatLng(45.261816, -94.123977);
+
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image,
+    title: "Lillian's"
+  });
+}
